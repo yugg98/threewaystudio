24abef8a8b24015dc67e979fac3235e475e7affc
Yug Gupta <70101980+yugg98@users.noreply.github.com>
2023-10-19 16:05:00 +0530

    done

---
+++ src/app/ourservices/marketing/page.tsx
@@ -0,0 +1,278 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <div className="relative overflow-hidden bg-gray-900 pb-32 pt-16">
+        <div className="relative">
+          <div className="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-2 lg:gap-24 lg:px-8">
+            <div className="mx-auto max-w-xl px-6 lg:mx-0 lg:max-w-none lg:px-0 lg:py-16">
+              <div>
+                <div>
+                  <span className="flex h-12 w-12 items-center justify-center rounded-xl bg-indigo-600">
+                    <svg
+                      className="h-8 w-8 text-white"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H6.911a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661z"
+                      />
+                    </svg>
+                  </span>
+                </div>
+                <div className="mt-6">
+                  <h1 className="text-xl mb-2 tracking-tight text-white">
+                    Community Management
+                  </h1>
+                  <h2 className="text-3xl font-bold tracking-tight text-white">
+                    Nurture Thriving Communities with our Expert Moderators,
+                    From 0 to 10x Strong in 3 months.
+                  </h2>
+                  <p className="mt-4  text-gray-200">
+                    At Threeway Studio, we believe in creating your community on
+                    Day 0 and building your product on Day 1. Our arsenal
+                    includes dynamic Nitro features, ensuring a captivating
+                    experience that keeps your members engaged and excited. With
+                    advanced AI-powered tools & Bots at our disposal, we harness
+                    the power of automation to streamline processes. From
+                    managing & nurturing Discord community & Telegram channel to
+                    moderating discussions, organising AMAs events, hosting
+                    Twitter spaces and facilitating seamless communication
+                    between community members and your project's team, we have
+                    got you covered. With us by your side, you can unlock the
+                    potential of your community and watch it flourish in no
+                    time.{" "}
+                  </p>
+                  <div className="mt-6">
+                    <a
+                      href="#"
+                      className="inline-flex rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
+                    >
+                      Get started
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-12 sm:mt-16 lg:mt-0">
+              <div className="-mr-48 pl-6 md:-mr-16 lg:relative lg:m-0 lg:h-full lg:px-0">
+                <img
+                  className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+                  src="https://tailwindui.com/img/component-images/inbox-app-screenshot-1.jpg"
+                  alt="Inbox user interface"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mt-24">
+          <div className="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-2 lg:gap-24 lg:px-8">
+            <div className="mx-auto max-w-xl px-6 lg:col-start-2 lg:mx-0 lg:max-w-none lg:px-0 lg:py-32">
+              <div>
+                <div>
+                  <span className="flex h-12 w-12 items-center justify-center rounded-xl bg-indigo-600">
+                    <svg
+                      className="h-8 w-8 text-white"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
+                      />
+                    </svg>
+                  </span>
+                </div>
+                <div className="mt-6">
+                  <h1 className="text-xl mb-2 tracking-tight text-white">
+                    Social Media Management
+                  </h1>
+                  <h2 className="text-3xl font-bold tracking-tight text-white">
+                    Be a Trendsetter & Empower your Brand Influence with our
+                    experienced marketing mavericks.
+                  </h2>
+                  <p className="mt-4  text-gray-200">
+                    At Threeway Studio, we're not just social media experts,
+                    we're Web3 Social Sorcerers. Our experienced Social Media
+                    Maestros are here to help you become a trendsetter, amplify
+                    your brand awareness and captivate connections making a
+                    lasting impact on your target audience. Whether it's
+                    Twitter, Snapchat, LinkedIn, or Instagram, we leverage the
+                    power of Internal tools like NFT Art engine, AR-VR
+                    lenses/filters, experienced moderators & schillers & and
+                    more to supercharge your social media presence. With our
+                    comprehensive monitoring, tracking, analysis, and reporting
+                    capabilities, we ensure that your social media performance
+                    is always optimised for success.
+                  </p>{" "}
+                  <div className="mt-6">
+                    <a
+                      href="#"
+                      className="inline-flex rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
+                    >
+                      Get started
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-12 sm:mt-16 lg:col-start-1 lg:mt-0">
+              <div className="-ml-48 pr-6 md:-ml-16 lg:relative lg:m-0 lg:h-full lg:px-0">
+                <img
+                  className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
+                  src="https://tailwindui.com/img/component-images/inbox-app-screenshot-2.jpg"
+                  alt="Customer profile user interface"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="relative">
+          <div className="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-2 lg:gap-24 lg:px-8">
+            <div className="mx-auto max-w-xl px-6 lg:mx-0 lg:max-w-none lg:px-0 lg:py-16">
+              <div>
+                <div>
+                  <span className="flex h-12 w-12 items-center justify-center rounded-xl bg-indigo-600">
+                    <svg
+                      className="h-8 w-8 text-white"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H6.911a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661z"
+                      />
+                    </svg>
+                  </span>
+                </div>
+                <div className="mt-6">
+                  <h1 className="text-xl mb-2 tracking-tight text-white">
+                    Community Management
+                  </h1>
+                  <h2 className="text-3xl font-bold tracking-tight text-white">
+                    Nurture Thriving Communities with our Expert Moderators,
+                    From 0 to 10x Strong in 3 months.
+                  </h2>
+                  <p className="mt-4  text-gray-200">
+                    At Threeway Studio, we believe in creating your community on
+                    Day 0 and building your product on Day 1. Our arsenal
+                    includes dynamic Nitro features, ensuring a captivating
+                    experience that keeps your members engaged and excited. With
+                    advanced AI-powered tools & Bots at our disposal, we harness
+                    the power of automation to streamline processes. From
+                    managing & nurturing Discord community & Telegram channel to
+                    moderating discussions, organising AMAs events, hosting
+                    Twitter spaces and facilitating seamless communication
+                    between community members and your project's team, we have
+                    got you covered. With us by your side, you can unlock the
+                    potential of your community and watch it flourish in no
+                    time.{" "}
+                  </p>
+                  <div className="mt-6">
+                    <a
+                      href="#"
+                      className="inline-flex rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
+                    >
+                      Get started
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-12 sm:mt-16 lg:mt-0">
+              <div className="-mr-48 pl-6 md:-mr-16 lg:relative lg:m-0 lg:h-full lg:px-0">
+                <img
+                  className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+                  src="https://tailwindui.com/img/component-images/inbox-app-screenshot-1.jpg"
+                  alt="Inbox user interface"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mt-24">
+          <div className="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-2 lg:gap-24 lg:px-8">
+            <div className="mx-auto max-w-xl px-6 lg:col-start-2 lg:mx-0 lg:max-w-none lg:px-0 lg:py-32">
+              <div>
+                <div>
+                  <span className="flex h-12 w-12 items-center justify-center rounded-xl bg-indigo-600">
+                    <svg
+                      className="h-8 w-8 text-white"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
+                      />
+                    </svg>
+                  </span>
+                </div>
+                <div className="mt-6">
+                  <h1 className="text-xl mb-2 tracking-tight text-white">
+                    Social Media Management
+                  </h1>
+                  <h2 className="text-3xl font-bold tracking-tight text-white">
+                    Be a Trendsetter & Empower your Brand Influence with our
+                    experienced marketing mavericks.
+                  </h2>
+                  <p className="mt-4  text-gray-200">
+                    At Threeway Studio, we're not just social media experts,
+                    we're Web3 Social Sorcerers. Our experienced Social Media
+                    Maestros are here to help you become a trendsetter, amplify
+                    your brand awareness and captivate connections making a
+                    lasting impact on your target audience. Whether it's
+                    Twitter, Snapchat, LinkedIn, or Instagram, we leverage the
+                    power of Internal tools like NFT Art engine, AR-VR
+                    lenses/filters, experienced moderators & schillers & and
+                    more to supercharge your social media presence. With our
+                    comprehensive monitoring, tracking, analysis, and reporting
+                    capabilities, we ensure that your social media performance
+                    is always optimised for success.
+                  </p>{" "}
+                  <div className="mt-6">
+                    <a
+                      href="#"
+                      className="inline-flex rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
+                    >
+                      Get started
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-12 sm:mt-16 lg:col-start-1 lg:mt-0">
+              <div className="-ml-48 pr-6 md:-ml-16 lg:relative lg:m-0 lg:h-full lg:px-0">
+                <img
+                  className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
+                  src="https://tailwindui.com/img/component-images/inbox-app-screenshot-2.jpg"
+                  alt="Customer profile user interface"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
