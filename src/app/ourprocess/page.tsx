980156777300a4e827b12091118af3345564b14c
Yug Gupta <70101980+yugg98@users.noreply.github.com>
2023-10-31 20:49:32 +0530

    done

--- src/app/ourprocess/page.tsx
+++ src/app/ourprocess/page.tsx
@@ -3,79 +3,79 @@ import React from "react";
 
 const page = () => {
   return (
-    <div>
+    <div className="bg-black ">
       <Process />
       <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="1085"
-          height="745"
-          viewBox="0 0 1085 745"
-          className="max-w-full absolute left-0 z-[-1]"
-          fill="none"
-        >
-          <g filter="url(#filter0_f_153_1464)">
-            <circle cx="463" cy="622" r="123" fill="#EC79F5" />
-          </g>
-          <defs>
-            <filter
-              id="filter0_f_153_1464"
-              x="-159"
-              y="0"
-              width="1244"
-              height="1244"
-              filterUnits="userSpaceOnUse"
-              color-interpolation-filters="sRGB"
-            >
-              <feFlood flood-opacity="0" result="BackgroundImageFix" />
-              <feBlend
-                mode="normal"
-                in="SourceGraphic"
-                in2="BackgroundImageFix"
-                result="shape"
-              />
-              <feGaussianBlur
-                stdDeviation="249.5"
-                result="effect1_foregroundBlur_153_1464"
-              />
-            </filter>
-          </defs>
-        </svg>
-        <svg
-          className="max-w-full absolute right-0 top-0 z-[-1]"
-          xmlns="http://www.w3.org/2000/svg"
-          width="663"
-          height="765"
-          viewBox="0 0 663 765"
-          fill="none"
-        >
-          <g filter="url(#filter0_f_153_1463)">
-            <circle cx="478" cy="287" r="79" fill="#DCFA6C" />
-          </g>
-          <defs>
-            <filter
-              id="filter0_f_153_1463"
-              x="0"
-              y="-191"
-              width="956"
-              height="956"
-              filterUnits="userSpaceOnUse"
-              color-interpolation-filters="sRGB"
-            >
-              <feFlood flood-opacity="0" result="BackgroundImageFix" />
-              <feBlend
-                mode="normal"
-                in="SourceGraphic"
-                in2="BackgroundImageFix"
-                result="shape"
-              />
-              <feGaussianBlur
-                stdDeviation="199.5"
-                result="effect1_foregroundBlur_153_1463"
-              />
-            </filter>
-          </defs>
-        </svg>
-      <div className="mb-24">
+        xmlns="http://www.w3.org/2000/svg"
+        width="1085"
+        height="745"
+        viewBox="0 0 1085 745"
+        className="max-w-full absolute top-0 left-0 z-1"
+        fill="none"
+      >
+        <g filter="url(#filter0_f_153_1464)">
+          <circle cx="463" cy="622" r="123" fill="#EC79F5" />
+        </g>
+        <defs>
+          <filter
+            id="filter0_f_153_1464"
+            x="-159"
+            y="0"
+            width="1244"
+            height="1244"
+            filterUnits="userSpaceOnUse"
+            color-interpolation-filters="sRGB"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              result="shape"
+            />
+            <feGaussianBlur
+              stdDeviation="249.5"
+              result="effect1_foregroundBlur_153_1464"
+            />
+          </filter>
+        </defs>
+      </svg>
+      <svg
+        className="max-w-full absolute right-0 top-0 z-[1]"
+        xmlns="http://www.w3.org/2000/svg"
+        width="663"
+        height="765"
+        viewBox="0 0 663 765"
+        fill="none"
+      >
+        <g filter="url(#filter0_f_153_1463)">
+          <circle cx="478" cy="287" r="79" fill="#DCFA6C" />
+        </g>
+        <defs>
+          <filter
+            id="filter0_f_153_1463"
+            x="0"
+            y="-191"
+            width="956"
+            height="956"
+            filterUnits="userSpaceOnUse"
+            color-interpolation-filters="sRGB"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              result="shape"
+            />
+            <feGaussianBlur
+              stdDeviation="199.5"
+              result="effect1_foregroundBlur_153_1463"
+            />
+          </filter>
+        </defs>
+      </svg>
+      <div className="">
         <h1 className="mb-4 mt-6 text-2xl text-center text-white font-bold md:text-5xl">
           Our Process
         </h1>
@@ -83,73 +83,64 @@ const page = () => {
           <div className="grid grid-cols-1 items-center gap-8 sm:gap-20 lg:grid-cols-2">
             <div className="max-w-[720px]">
               <h3 className="mb-4 mt-6 text-2xl text-white font-bold md:text-3xl">
-              Onboard
+                Onboard
               </h3>
               <p className="mb-6 max-w-[480px] text-white md:mb-10 lg:mb-12">
-              We begin with the initial ideas via rigorous interviews and sprint planning to turn your concept into unique ideas.
+                We begin with the initial ideas via rigorous interviews and
+                sprint planning to turn your concept into unique ideas.
               </p>
-              <a
-                href="#"
-                className="inline-block rounded-lg border border-solid border-[#6B21A8] bg-[#6B21A8] px-6 py-4 text-white text-center font-bold  transition hover:border-black hover:bg-white"
-              >
-                Contact us
-              </a>
+             
             </div>
-            <div>
+            <div className="order-first md:order-last">
               <img
-               src="https://ik.imagekit.io/shishirtiwari23/Threeway/HeroShot_ObvLcLQlI.gif?tr=w-828"
+                src="https://ik.imagekit.io/shishirtiwari23/Threeway/HeroShot_ObvLcLQlI.gif?tr=w-828"
                 alt="Features Image"
-                className="mx-auto inline-block h-full w-full max-w- rounded-2xl object-cover"
+                className="mx-auto \ h-full w-full max-w- rounded-2xl object-cover"
               />
             </div>
           </div>
         </div>
-        <div className="mx-auto w-full max-w-7xl px-5 py-16 md:px-10 md:py-24 lg:py-8">
+        <div className="mx-auto  w-full max-w-7xl px-5 py-16 md:px-10 md:py-24 lg:py-8">
           <div className="grid grid-cols-1 items-center gap-8 sm:gap-20 lg:grid-cols-2">
             <div>
-              <img
-                 src="img/develop.png"
-                alt="Features Image"
-                className="mx-auto inline-block h-full w-full max-w- rounded-2xl object-cover"
-              />
-            </div>
-            <div className="max-w-[720px]">
-              <h3 className="mb-4 mt-6 text-2xl text-white font-bold md:text-3xl">
-              Strategise
-              </h3>
-              <p className="mb-6 max-w-[480px] text-white md:mb-10 lg:mb-12">
-              Here we focus on dissecting the original idea and realizing it into a feasible & scalable product after conducting extensive research on market opportunities, risks, and constraints.
-              </p>
-              <a
-                href="#"
-                className="inline-block rounded-lg border border-solid border-[#6B21A8] bg-[#6B21A8] px-6 py-4 text-white text-center font-bold  transition hover:border-black hover:bg-white"
-              >
-                Learn More
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="mx-auto w-full max-w-7xl px-5 py-16 md:px-10 md:py-24 lg:py-8">
-          <div className="grid grid-cols-1 items-center gap-8 sm:gap-20 lg:grid-cols-2">
-            <div className="max-w-[720px]">
-              <h3 className="mb-4 mt-6 text-2xl text-white font-bold md:text-3xl">
-              Project Mapping 
-
-              </h3>
-              <p className="mb-6 max-w-[480px] text-white md:mb-10 lg:mb-12">
-              We plan & document every minute project detail from the initial model, explicit blueprint, and first MVP launch to getting the first customer on board.              </p>
-              <a
-                href="#"
-                className="inline-block rounded-lg border border-solid border-[#6B21A8] bg-[#6B21A8] px-6 py-4 text-white text-center font-bold  transition hover:border-black hover:bg-white"
-              >
-                Contact us
-              </a>
-            </div>
-            <div>
-              <img
+            <img
                 src="https://ik.imagekit.io/shishirtiwari23/Threeway/gif_K2J3n5b4p.gif?tr=w-384 1x, https://ik.imagekit.io/shishirtiwari23/Threeway/gif_K2J3n5b4p.gif?tr=w-640 2x"
                 alt="Features Image"
-                className=" inline-block h-full w-full max-w-  rounded-2xl object-cover"
+                className=" -block h-full w-full max-w-  rounded-2xl object-cover"
+              />
+            </div>
+            <div className="max-w-[720px]">
+              <h3 className="mb-4 mt-6 text-2xl text-white font-bold md:text-3xl">
+                Strategise
+              </h3>
+              <p className="mb-6 max-w-[480px] text-white md:mb-10 lg:mb-12">
+                Here we focus on dissecting the original idea and realizing it
+                into a feasible & scalable product after conducting extensive
+                research on market opportunities, risks, and constraints.
+              </p>
+              
+            </div>
+          </div>
+        </div>
+        <div className="mx-auto w-full max-w-7xl px-5 py-16 md:px-10 md:py-24 lg:py-8">
+          <div className="grid grid-cols-1 items-center gap-8 sm:gap-20 lg:grid-cols-2">
+            <div className="max-w-[720px]">
+              <h3 className="mb-4 mt-6 text-2xl text-white font-bold md:text-3xl">
+                Project Mapping
+              </h3>
+              <p className="mb-6 max-w-[480px] text-white md:mb-10 lg:mb-12">
+                We plan & document every minute project detail from the initial
+                model, explicit blueprint, and first MVP launch to getting the
+                first customer on board.{" "}
+              </p>
+             
+            </div>
+            <div className="order-first md:order-last">
+              
+               <img
+                src="https://ik.imagekit.io/shishirtiwari23/Threeway/Compressed__gOZ54lp3.gif?tr=w-640"
+                alt="Features Image"
+                className="mx-auto -block h-full w-full max-w- rounded-2xl object-cover"
               />
             </div>
           </div>
@@ -158,25 +149,20 @@ const page = () => {
           <div className="grid grid-cols-1 items-center gap-8 sm:gap-20 lg:grid-cols-2">
             <div>
               <img
-               
-               src="https://ik.imagekit.io/shishirtiwari23/Threeway/marketImage_EzMLIKVx5.gif?tr=w-640"
-                
+                src="https://ik.imagekit.io/shishirtiwari23/Threeway/marketImage_EzMLIKVx5.gif?tr=w-640"
                 alt="Features Image"
-                className="mx-auto inline-block h-full w-full  rounded-2xl object-cover"
+                className="mx-auto -block h-full w-full  rounded-2xl object-cover"
               />
             </div>
             <div className="max-w-[720px]">
               <h3 className="mb-4 mt-6 text-2xl text-white font-bold md:text-3xl">
-              Prototyping
+                Prototyping
               </h3>
               <p className="mb-6 max-w-[480px] text-white md:mb-10 lg:mb-12">
-              Our design team starts building on the ideas and creates a design mockup of the final product.              </p>
-              <a
-                href="#"
-                className="inline-block rounded-lg border border-solid border-[#6B21A8] bg-[#6B21A8] px-6 py-4 text-white text-center font-bold  transition hover:border-black hover:bg-white"
-              >
-                Learn More
-              </a>
+                Our design team starts building on the ideas and creates a
+                design mockup of the final product.{" "}
+              </p>
+              
             </div>
           </div>
         </div>
@@ -184,23 +170,20 @@ const page = () => {
           <div className="grid grid-cols-1 items-center gap-8 sm:gap-20 lg:grid-cols-2">
             <div className="max-w-[720px]">
               <h3 className="mb-4 mt-6 text-2xl text-white font-bold md:text-3xl">
-              Brand Message Development 
+                Brand Message Development
               </h3>
               <p className="mb-6 max-w-[480px] text-white md:mb-10 lg:mb-12">
-              Parallel to building the prototype, we also begin to draft the perfect compelling messaging about the product and the phases of marketing strategy.              </p>
-              <a
-                href="#"
-                className="inline-block rounded-lg border border-solid border-[#6B21A8] bg-[#6B21A8] px-6 py-4 text-white text-center font-bold  transition hover:border-black hover:bg-white"
-              >
-                Contact us
-              </a>
+                Parallel to building the prototype, we also begin to draft the
+                perfect compelling messaging about the product and the phases of
+                marketing strategy.{" "}
+              </p>
+             
             </div>
-            <div>
+            <div className="order-first md:order-last">
               <img
                 src="img/img.png"
-                
                 alt="Features Image"
-                className="mx-auto inline-block h-full w-full max-w-xs rounded-2xl object-cover"
+                className="mx-auto order-first md:order-last  h-full w-full max-w-[230px] rounded-2xl object-cover"
               />
             </div>
           </div>
@@ -209,48 +192,41 @@ const page = () => {
           <div className="grid grid-cols-1 items-center gap-8 sm:gap-20 lg:grid-cols-2">
             <div>
               <img
-              src="img/img2.png"
-               
+                src="img/img2.png"
                 alt="Features Image"
-                className="mx-auto inline-block h-full w-full max-w-xs rounded-2xl object-cover"
+                className="mx-auto -block h-full w-full max-w-[230px]  rounded-2xl object-cover"
               />
             </div>
             <div className="max-w-[720px]">
               <h3 className="mb-4 mt-6 text-2xl text-white font-bold md:text-3xl">
-              Engineering
+                Engineering
               </h3>
               <p className="mb-6 max-w-[480px] text-white md:mb-10 lg:mb-12">
-              Our team gets to rigorous product development & builds the first minimum viable product (MVP).              </p>
-              <a
-                href="#"
-                className="inline-block rounded-lg border border-solid border-[#6B21A8] bg-[#6B21A8] px-6 py-4 text-white text-center font-bold  transition hover:border-black hover:bg-white"
-              >
-                Learn More
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="mx-auto w-full max-w-7xl px-5 py-16 md:px-10 md:py-24 lg:py-8">
-          <div className="grid grid-cols-1 items-center gap-8 sm:gap-20 lg:grid-cols-2">
-            <div className="max-w-[720px]">
-              <h3 className="mb-4 mt-6 text-2xl text-white font-bold md:text-3xl">
-              Beta Testing/ Validation/Product Evaluation 
-              </h3>
-              <p className="mb-6 max-w-[480px] text-white md:mb-10 lg:mb-12">
-              The product is turned over to the testing team which helps us fix any issues that come up and make our product ready for delivery.              </p>
-              <a
-                href="#"
-                className="inline-block rounded-lg border border-solid border-[#6B21A8] bg-[#6B21A8] px-6 py-4 text-white text-center font-bold  transition hover:border-black hover:bg-white"
-              >
-                Contact us
-              </a>
-            </div>
-            <div>
-              <img
-             src="monshot.gif"
+                Our team gets to rigorous product development & builds the first
+                minimum viable product (MVP).{" "}
+              </p>
               
+            </div>
+          </div>
+        </div>
+        <div className="mx-auto w-full max-w-7xl px-5 py-16 md:px-10 md:py-24 lg:py-8">
+          <div className="grid grid-cols-1 items-center gap-8 sm:gap-20 lg:grid-cols-2">
+            <div className="max-w-[720px]">
+              <h3 className="mb-4 mt-6 text-2xl text-white font-bold md:text-3xl">
+                Beta Testing/ Validation/Product Evaluation
+              </h3>
+              <p className="mb-6 max-w-[480px] text-white md:mb-10 lg:mb-12">
+                The product is turned over to the testing team which helps us
+                fix any issues that come up and make our product ready for
+                delivery.{" "}
+              </p>
+             
+            </div>
+            <div className="order-first md:order-last">
+              <img
+                src="monshot.gif"
                 alt="Features Image"
-                className="mx-auto inline-block h-full w-full max-w-xs rounded-2xl object-cover"
+                className="mx-auto  order-first md:order-last\ h-full w-full max-w-xs rounded-2xl object-cover"
               />
             </div>
           </div>
@@ -259,23 +235,23 @@ const page = () => {
           <div className="grid grid-cols-1 items-center gap-8 sm:gap-20 lg:grid-cols-2">
             <div>
               <img
-              src="/img4.jpeg"
+                src="/img4.jpeg"
                 alt="Features Image"
-                className="mx-auto inline-block h-full w-full max-w-xs rounded-2xl object-cover"
+                className="mx-auto\ h-full w-full max-w-xs rounded-2xl object-cover"
               />
             </div>
             <div className="max-w-[720px]">
               <h3 className="mb-4 mt-6 text-2xl text-white font-bold md:text-3xl">
-              Launch
+                Launch
               </h3>
               <p className="mb-6 max-w-[480px] text-white md:mb-10 lg:mb-12">
-              We&apos;re ready to launch the product to the public after running successful pre-launch campaigns, understanding the market interest, building buyer personas, & drafting marketing strategies. The next step is to get the first customer & help you scale your business.              </p>
-              <a
-                href="#"
-                className="inline-block rounded-lg border border-solid border-[#6B21A8] bg-[#6B21A8] px-6 py-4 text-white text-center font-bold  transition hover:border-black hover:bg-white"
-              >
-                Learn More
-              </a>
+                We&apos;re ready to launch the product to the public after
+                running successful pre-launch campaigns, understanding the
+                market interest, building buyer personas, & drafting marketing
+                strategies. The next step is to get the first customer & help
+                you scale your business.{" "}
+              </p>
+             
             </div>
           </div>
         </div>
