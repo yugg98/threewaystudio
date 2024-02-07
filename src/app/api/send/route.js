5404bdcad193be3fae80dd65ee6a952b6c39d67f
Yug Gupta <70101980+yugg98@users.noreply.github.com>
2024-02-07 11:04:58 +0530

    done

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -2,14 +2,14 @@ import { EmailTemplate } from '../../../components/EmailTemplate';
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
-const resend = new Resend("re_gKUcqhf1_NgnKwG692GtWi4tMvVi94MFc");
+const resend = new Resend("re_cp5UwVMH_7xUGqg8EiQMffQV2rr78Wf5P");
 
 export async function POST(req) {
   const {firstname,lastname,email,phonenumber,message} = await req.json()
   try {
     const data = await resend.emails.send({
-        from: 'Acme <onboarding@resend.dev>',
-        to: ['contact@thedigitalloom.com'],
+      from: 'onboarding@resend.dev',
+      to: 'threewaystudiofirm@gmail.com',
         subject: 'New Request',
         react: EmailTemplate({ firstname: firstname,lastname:lastname,email:email,phonenumber:phonenumber,message:message }),
     });
