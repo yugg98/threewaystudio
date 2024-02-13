3ae823fb0da141a92c1b34348506bc2f5c7e774f
Yug Gupta <70101980+yugg98@users.noreply.github.com>
2024-02-13 22:58:49 +0530

    done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -232,7 +232,7 @@ export default function Example() {
         </div>
         <div className="mt-16 border-t border-white/10 pt-8 sm:mt-20 lg:mt-24">
           <p className="text-xs leading-5 text-gray-400">
-            &copy; 2023 Your Company, Inc. All rights reserved.
+            &copy; 2024 Threeway Studio, Inc. All rights reserved.
           </p>
         </div>
       </div>
